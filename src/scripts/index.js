8a970132aa97c9215867165c33b4bab1804c5154
Omar <omar0940@gmail.com>
2021-05-10 10:40:00 +0200

    Digital Clock app

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,1 +1,28 @@
+function clock() {
+  let date = new Date();
+  let h = date.getHours();
+  let m = date.getMinutes();
+  let s = date.getSeconds();
+  let meridian = "AM";
 
+  if (h == 0) {
+    h = 12;
+  }
+
+  if (h > 12) {
+    h = h - 12;
+    meridian = "PM";
+  }
+
+  h = h < 10 ? "0" + h : h;
+  m = m < 10 ? "0" + m : m;
+  s = s < 10 ? "0" + s : s;
+
+  let time = h + ":" + m + ":" + s + " " + meridian;
+  document.getElementById("MyClockDisplay").innerText = time;
+  document.getElementById("MyClockDisplay").textContent = time;
+
+  setTimeout(clock, 1000);
+}
+
+clock();
